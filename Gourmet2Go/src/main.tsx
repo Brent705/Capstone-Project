308caa30683aed67310ffe3807375e3144d032be
iraana <24048851@saultcollege.ca>
2026-01-20 17:58:20 -0500

    initial commit

---
+++ Gourmet2Go/src/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+import { BrowserRouter as Router } from 'react-router'; 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { AuthProvider } from './context/AuthContext.tsx';
+
+const client = new QueryClient();  // A QueryClient instance created, used to cache data
+
+// Renders in the root element in index.html and wraps the App component with necessary providers
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+      {/* <AuthProvider> */}
+        <Router>
+          <App /> 
+        </Router>
+      {/* </AuthProvider> */}
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
